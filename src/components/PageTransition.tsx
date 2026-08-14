994ec18d3535e7cdd4af2a5a878bd3191c9e2d7a
yaswanthmannepalli-dev <yaswanthmannepalli-dev@github.com>
2026-08-14 11:48:56 +0530

    UI: Reduce page transition animation duration to 0.1s for faster navigation

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -38,7 +38,7 @@ export default function PageTransition({ children }: { children: React.ReactNode
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -15 }}
           transition={{
-            duration: 0.5,
+            duration: 0.1,
             ease: [0.22, 1, 0.36, 1],
           }}
           className="flex flex-col flex-grow"
